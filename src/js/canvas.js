ce85590653711d30cb9357ab59f463a9f5b55b9f
Azuka <97498213+Knavish1@users.noreply.github.com>
2022-05-18 16:18:29 -0700

    A few updates as git commit command was not working.
    A few errors, could not get end marker to load the image so could not test the alert game win function.

--- src/js/canvas.js
+++ src/js/canvas.js
@@ -1,6 +1,8 @@
-import platform from '../img/platform3.png'
+import platform from '../img/platform4.png'
 import sky  from '../img/sky.png'
-import hills from '../img/far-grounds.png'
+import hills from '../img/hills.png'
+import hills2 from '../img/hills2.png'
+import ender from '../img/end1.png'
 
 
 
@@ -8,12 +10,14 @@ const canvas = document.querySelector('canvas')
 const c = canvas.getContext('2d')
 
 canvas.width = 1024
-canvas.height = 576
+canvas.height = 410
 
+const jumpcount = 0
 const gravity = 0.5
 
 class Mooninite {
     constructor(){
+        this.speed = 10
         this.position = {
             x: 200, 
             y: 100
@@ -38,10 +42,8 @@ class Mooninite {
         
         if(this.position.y + this.height + this.velocity.y <= canvas.height)
              this.velocity.y += gravity
-        else this.velocity.y = 0
-             
-
         
+                     
     }
 }
 
@@ -79,6 +81,23 @@ class scenery {
       c.drawImage(this.image, this.position.x, this.position.y)
   }
 }
+class Goal {
+    constructor({x, y, image}){
+        this.position = {
+            x,
+            y
+        }
+  
+        this.image = image
+        this.width = image.width
+        this.height = image.height
+  
+         
+    }
+    manifest(){
+        c.drawImage(this.image, this.position.x, this.position.y)
+    }
+}
 
 const image = new Image()
 
@@ -88,15 +107,16 @@ image.src = imageSrc
 return image
 }
 
-console.log(image)
+let endMarker = [new Goal({x: 150, y: 400, image: createImage(ender)}), new Goal({x:3000, y: 400, image: createImage(ender)})]
 
-const player = new Mooninite()
+let player = new Mooninite()
 
-const platformImage = createImage(platform)
+let platformImage = createImage(platform)
 
-const platforms = [new MoonRocks({x: -1, y: 540, image: platformImage}), new MoonRocks({x: platformImage.width -2, y: 540, image: platformImage})]
+let platforms = [new MoonRocks({x: -1, y: 380, image: platformImage}), new MoonRocks({x: platformImage.width -2, y: 380, image: platformImage}), new MoonRocks({x: platformImage.width*2 -10, y: 380, image: platformImage}),new MoonRocks({x: platformImage.width*3 -12, y: 380, image: platformImage}), new MoonRocks({x: platformImage.width*4 -14, y: 380, image: platformImage}),new MoonRocks({x: platformImage.width*5 + 50, y: 380, image: platformImage})]
 
-const behind = [new scenery ({x: 0 , y: 0, image: createImage(sky)}), new scenery ({x: 100, y: 0, image:createImage(sky)})]
+let behind = [new scenery ({x: -2 , y: 0, image: createImage(sky)}), new scenery ({x: 1400, y: 0, image:createImage(sky)}), new scenery ({x: 2100, y: 0, image:createImage(sky)}), new scenery ({x: 700, y: 0, image:createImage(sky)}), new scenery ({x: 1200, y: 245, image:createImage(hills)}), new scenery({x: 450, y: 245, image:createImage(hills)})]
+
 
 const keys = {
     right: {
@@ -104,11 +124,33 @@ const keys = {
     },
     left: {
         pressed: false
+    },
+    up: {
+        pressed: false
     }
 }
 
 let scrollOffset = 0
+console.log(image)
 
+function init(){
+
+ // supposed to create new image that toggles game end, but has not shown up   
+ endMarker = [new Goal({x: 150, y: 400, image: createImage(ender)}), new Goal({x: 3000, y: 250, image: createImage(ender)})]
+
+ player = new Mooninite()
+
+ platformImage = createImage(platform)
+
+ platforms = [new MoonRocks({x: platformImage.width*8 + 100, y: 310, image: createImage(hills2)}), new MoonRocks({x: -1, y: 400, image: platformImage}), new MoonRocks({x: platformImage.width -2, y: 400, image: platformImage}), new MoonRocks({x: platformImage.width*2 -10, y: 400, image: platformImage}),new MoonRocks({x: platformImage.width*3 -12, y: 400, image: platformImage}), new MoonRocks({x: platformImage.width*4 -14, y: 400, image: platformImage}),new MoonRocks({x: platformImage.width*5 + 200, y: 400, image: platformImage}), new MoonRocks({x: platformImage.width*6 + 200, y: 400, image: platformImage}), new MoonRocks({x: platformImage.width*7 +200, y: 400, image: platformImage}, new MoonRocks({x: platformImage.width*8, y: 400, image: platformImage})), new MoonRocks({x: platformImage.width*10 + 200, y: 400, image: platformImage}), new MoonRocks({x: platformImage.width*11 + 200, y: 400, image: platformImage}), new MoonRocks({x: platformImage.width*12 + 200, y: 400, image: platformImage}), new MoonRocks({x: platformImage.width*13 + 200, y: 400, image: platformImage}), new MoonRocks({x: platformImage.width*14 + 200, y: 400, image: platformImage}), new MoonRocks({x: platformImage.width*15 + 200, y: 400, image: platformImage}), new MoonRocks({x: platformImage.width*17 , y: 300, image: platformImage}), new MoonRocks({x: platformImage.width*18.5 , y: 200, image: platformImage}), new MoonRocks({x: platformImage.width*21 + 200, y: 400, image: platformImage}), new MoonRocks({x: platformImage.width*22 + 200, y: 400, image: platformImage}), new MoonRocks({x: platformImage.width*23 + 200, y: 400, image: platformImage}), new MoonRocks({x: platformImage.width*23 + 200, y: 400, image: platformImage}), new MoonRocks({x: platformImage.width*24 + 200, y: 400, image: platformImage}),new MoonRocks({x: platformImage.width*25 + 200, y: 400, image: platformImage}),new MoonRocks({x: platformImage.width*26 + 200, y: 400, image: platformImage}), new MoonRocks({x: platformImage.width*27 + 200, y: 400, image: platformImage}),  new MoonRocks({x: platformImage.width*28 + 200, y: 400, image: platformImage}),  new MoonRocks({x: platformImage.width*29 + 200, y: 400, image: platformImage}),  new MoonRocks({x: platformImage.width*30 + 200, y: 400, image: platformImage})    ]
+
+ behind = [new scenery ({x: -2 , y: 0, image: createImage(sky)}), new scenery ({x: 700, y: 0, image:createImage(sky)}), new scenery ({x: 1200, y: 280, image:createImage(hills)}), new scenery({x: 1500, y: 280, image:createImage(hills)}),  new scenery({x: 1400, y: 0, image:createImage(sky)}),new scenery({x: 2100, y: 0, image:createImage(sky)}),new scenery({x: 2800, y: 0, image:createImage(sky)}),new scenery({x: 3500, y: 0, image:createImage(sky)}), new scenery({x: 4200, y: 0, image:createImage(sky)}), new scenery({x: 4900, y: 0, image:createImage(sky)}) ]
+ 
+
+
+
+scrollOffset = 0
+}
 // recursive canvas drawing/ moving user
 function animate() {
     requestAnimationFrame(animate)
@@ -126,25 +168,29 @@ function animate() {
     player.update()
 
     if(keys.right.pressed && player.position.x < 400){
-        player.velocity.x = 5
-    } else if (keys.left.pressed && player.position.x > 100){
-        player.velocity.x = -5
+        player.velocity.x = player.speed
+    } else if ((keys.left.pressed && player.position.x > 100) || keys.left.pressed && scrollOffset ===0 && player.position.x > 0){
+        player.velocity.x = -player.speed
     } else {
         player.velocity.x = 0
 
         if (keys.right.pressed){
-            scrollOffset +=5
+            scrollOffset += player.speed
             platforms.forEach((platform) => {
-            platform.position.x -= 5
+            platform.position.x -= player.speed
         })
-
+        behind.forEach(scenery => {
+            scenery.position.x -= player.speed * .66
+        })
            
-        } else if (keys.left.pressed) {
-            scrollOffset -= 5
+        } else if (keys.left.pressed && scrollOffset > 0) {
+            scrollOffset -= player.speed
             platforms.forEach((platform) => {
-            platform.position.x += 5
+            platform.position.x += player.speed
         })
-
+        behind.forEach(scenery => {
+            scenery.position.x += player.speed * .66
+        })
             
         }
     }
@@ -154,12 +200,21 @@ function animate() {
         player.velocity.y = 0
     }
 })
-
-if (scrollOffset >2000){
+   //ends game after certain reach
+    endMarker.forEach((ender) => {
+        if (player.position.y + player.height <= ender.position.y && player.position.y + player.height + player.velocity.y >= ender.position.y && player.position.x + player.width >= ender.position.x && player.position.x <= ender.position.x + platform.width){
+            alert(Finished+ '(jumpcount)')
+    }
+})
+// how to win
+if (scrollOffset > 3000){
     console.log('You Win!')
 }
+if(player.position.y > canvas.height)
+    init()
 }
 
+init()
 animate()
 // keyboard player move keyset
 window.addEventListener('keydown', ({key}) => {
@@ -171,7 +226,11 @@ window.addEventListener('keydown', ({key}) => {
             break
          case 'ArrowUp':
             console.log('up')
-            player.velocity.y -= 20
+            
+            if (keys.up.pressed = true)
+            player.velocity.y -= 10
+            jumpcount++
+            
             break
          case 'ArrowRight':
              keys.right.pressed = true
@@ -193,7 +252,7 @@ window.addEventListener('keyup', ({key}) => {
             break
          case 'ArrowUp':
             console.log('up')
-            player.velocity.y = 0
+            keys.up.pressed = true
             break
          case 'ArrowRight':
             keys.right.pressed = false
